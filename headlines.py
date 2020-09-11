5bbeb0f65a22b59827a266a53d8b1460ce8342a9
mostafaebeid <mostafa.ebeid84@gmail.com>
2020-09-11 13:07:12 +0200

    Done Weather

--- headlines.py
+++ headlines.py
@@ -1,7 +1,12 @@
 import feedparser
 from flask import Flask, render_template, request
+import json
+import urllib3, urllib.parse
+
 
 app = Flask(__name__)
+
+http = urllib3.PoolManager()
 
 RSS_FEEDS = {
             'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
@@ -10,16 +15,52 @@ RSS_FEEDS = {
             'iol': 'http://www.iol.co.za/cmlink/1.640'
             }
 
+DEFAULTS = {
+    'publication':'bbc',
+    'city': 'London,UK'
+    }
+
+
 @app.route("/")
-def get_news():
-    query = request.args.get("publication")
+def home():
+    #get customized headlines, based on user input or default
+    publication = request.args.get('publication')
+    if not publication:
+        publication = DEFAULTS['publication']
+    articles = get_news(publication)
+    #get customized weather based on user input or default
+    city = request.args.get('city')
+    if not city:
+        city = DEFAULTS['city']
+    weather = get_weather(city)
+
+    return render_template("home.html", articles=articles, weather=weather)
+
+def get_news(query):
     if not query or query.lower() not in RSS_FEEDS:
-        publication = "bbc"
+        publication = DEFAULTS['publication']
     else:
         publication = query.lower()
 
     feed = feedparser.parse(RSS_FEEDS[publication])
-    return render_template("home.html", articles=feed['entries'])
+    return feed['entries']
+
+def get_weather(query):
+    query = urllib.parse.quote(query)
+    api_key = '227b01cf861c1008f6186816d7fbe70e'
+    api_url = f"http://api.openweathermap.org/data/2.5/weather?q={query}&units=metric&appid={api_key}"
+    
+    r = http.request('GET', api_url)
+    parsed = json.loads(r.data.decode('utf-8'))
+    weather = None
+    if parsed.get("weather"):
+        weather = {"description":
+        parsed["weather"][0]["description"],
+        "temperature":parsed["main"]["temp"],
+        "city":parsed["name"],
+        "country":parsed['sys']['country']
+        }
+    return weather
 
 
 if __name__ == '__main__':
